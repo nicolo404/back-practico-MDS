92739688b783f9adc58b7b3fc648036017734df0
Nicolas Silva (Talcahuano) <nsilva@marinadelsol.cl>
2024-01-09 16:57:14 -0300

    obtener solo 1 usuario

--- Routes/RoutesUser.js
+++ Routes/RoutesUser.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post('/crear-usuario', ControllerUser.crearUsuario);
 router.get('/obtener-usuarios', ControllerUser.obtenerUsuarios);
+router.get('/obtener-usuario/:id', ControllerUser.obtenerUsuario);
 router.delete('/eliminar-usuario/:id', ControllerUser.eliminarUsuario);
 router.put('/actualizar-usuario/:id', ControllerUser.actualizarUsuario);
 
